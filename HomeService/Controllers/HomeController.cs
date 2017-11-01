5546c9a18bcbddf7eed1a4e0a14d31612df9bc1b
jamasu <suleiman.jama@dss.dep.no>
2017-11-01 22:59:06 +0100

    clean up cortana folder

--- HomeService/Controllers/HomeController.cs
+++ HomeService/Controllers/HomeController.cs
@@ -1,5 +1,4 @@
-﻿
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.NodeServices;
 using HomeService.Business;
@@ -18,6 +17,7 @@ namespace HomeService.Controllers
         private const string VeriSureAppFilePath = @"wwwroot\lib\Verisure\verisureApp.js";
         private const string APIToken = "f905040cdb3e4d9482aba2d35d3a2bae";
         private const string VoiceFile = @"Business\VoiceFile.txt";
+        private const string CognitiveServiceRun = @"C:\Users\pa_suja\Source\Repos\HomeService\VoiceCortana\bin\Debug\CognitiveServicesTTS.exe";
         public IActionResult Index()
         {
             return View();
@@ -61,29 +61,55 @@ namespace HomeService.Controllers
         public void WriteToVoiceFile(ClimateData[] climateDataObjecsToFile, AlarmStatus alarmData)
         {
             FileStream fstream = new FileStream(VoiceFile, FileMode.Truncate);
-
-            string fileContent = "";
-            fileContent += "Velkommen " + alarmData.Name; fileContent += Environment.NewLine;
-            fileContent += Environment.NewLine;
-
-
-            for (int i = 0; i < climateDataObjecsToFile.Length; i++)
-            {
-                fileContent += climateDataObjecsToFile[i].Location; fileContent += " ";
-                fileContent += " " + climateDataObjecsToFile[i].Temperature + " grader!."; fileContent += Environment.NewLine;
-            }
-
-
+            string fileContent = ClimateStatusCheck(climateDataObjecsToFile, alarmData);
             byte[] info = new UTF8Encoding(true).GetBytes(fileContent);
 
             fstream.Write(info, 0, fileContent.Length);
             fstream.Dispose();
         }
 
+        private static string ClimateStatusCheck(ClimateData[] climateDataObjecsToFile, AlarmStatus alarmData)
+        {
+            string fileContent = "";
+            fileContent += "Velkommen " + alarmData.Name; fileContent += Environment.NewLine;
+            fileContent += Environment.NewLine;
+            fileContent += "klokken er " + DateTime.Now.ToString("hh:mm"); fileContent += Environment.NewLine;
+            fileContent += "Utfører system sjekk !.";
+
+            for (int i = 0; i < climateDataObjecsToFile.Length; i++)
+            {
+                if (climateDataObjecsToFile[i].Temperature > 22 && climateDataObjecsToFile[i].Temperature > 0)
+                {
+                    fileContent += climateDataObjecsToFile[i].Location; fileContent += " ";
+                    fileContent += " " + climateDataObjecsToFile[i].Temperature + " grader!. for varmt!"; fileContent += Environment.NewLine;
+                }
+                else if (climateDataObjecsToFile[i].Temperature < 18 && climateDataObjecsToFile[i].Temperature > 0)
+                {
+                    fileContent += climateDataObjecsToFile[i].Location; fileContent += " ";
+                    fileContent += " " + climateDataObjecsToFile[i].Temperature + " grader!. for kaldt!"; fileContent += Environment.NewLine;
+                }
+                else if (climateDataObjecsToFile[i].Humidity > 55 && climateDataObjecsToFile[i].Humidity > 0)
+                {
+                    fileContent += climateDataObjecsToFile[i].Location; fileContent += " ";
+                    fileContent += " " + " Foktighetsnivå høy!" + climateDataObjecsToFile[i].Humidity; fileContent += Environment.NewLine;
+                }
+                else if (climateDataObjecsToFile[i].Humidity < 35 && climateDataObjecsToFile[i].Humidity > 0)
+                {
+                    fileContent += climateDataObjecsToFile[i].Location; fileContent += " ";
+                    fileContent += " " + " Foktighetsnivå lav!" + climateDataObjecsToFile[i].Humidity; fileContent += Environment.NewLine;
+                }
+                else
+                {
+                    fileContent += climateDataObjecsToFile[i].Location; fileContent += " OK. ";
+                }
+            }
+            return fileContent;
+        }
+
         public void StartVoiceProgram()
         {
             ProcessStartInfo psi = new ProcessStartInfo();
-            psi.FileName = @"C:\Users\pa_suja\Source\Repos\VoiceCortana\bin\Debug\CognitiveServicesTTS.exe";
+            psi.FileName = CognitiveServiceRun;
             psi.WorkingDirectory = System.IO.Path.GetDirectoryName(psi.FileName);
             Process.Start(psi);
         }
